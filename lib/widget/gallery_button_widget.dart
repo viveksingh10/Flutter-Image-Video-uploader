756aab19034288cd851c238409e312900496bd7d
aakashcode1708 <singhaakash601@gmail.com>
2021-08-03 15:11:54 +0530

    firebase added

--- lib/widget/gallery_button_widget.dart
+++ lib/widget/gallery_button_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_video_picker_example/model/media_source.dart';
@@ -15,6 +16,8 @@ class GalleryButtonWidget extends StatelessWidget {
       );
 
   Future pickGalleryMedia(BuildContext context) async {
+     final _storage = FirebaseStorage.instance;
+   
     final MediaSource source = ModalRoute.of(context).settings.arguments;
 
     final getMedia = source == MediaSource.image
@@ -23,6 +26,13 @@ class GalleryButtonWidget extends StatelessWidget {
 
     final media = await getMedia(source: ImageSource.gallery);
     final file = File(media.path);
+    if (media != null) {
+      var snapshot = await _storage
+          .ref()
+          .child('folder/imageName')
+          .putFile(file)
+          .onComplete;
+    }
 
     Navigator.of(context).pop(file);
   }
